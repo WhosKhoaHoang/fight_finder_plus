88ebe390a2092f754646ba2912aebfbb1003ca03
Khoa Hoang <whoskhoahoang@gmail.com>
2020-03-20 22:16:46 +0900

    Begin to implement search components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css"
+
+
+
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <form>
+                <input type="text" placeholder="Search..." />
+            </form>
+        );
+    }
+}
+
+class SearchModule extends React.Component {
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <SearchModule />,
+    document.getElementById("root")
+);
+
+//Simpler JSX syntax
+//const element = <h1>Hello, world</h1>;
+//ReactDOM.render(element, document.getElementById('root'));
