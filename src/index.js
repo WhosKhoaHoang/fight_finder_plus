2e5b4c9f49c7eb72a69fe0b821a58b257d4389a5
Khoa Hoang <whoskhoahoang@gmail.com>
2020-03-21 11:44:53 +0900

    Make simple results appear
    NOTE: Results are hard-coded and
          statically generated.

--- src/index.js
+++ src/index.js
@@ -3,30 +3,77 @@ import ReactDOM from "react-dom";
 import "./index.css"
 
 
-
-
-class SearchBar extends React.Component {
-    render() {
-        return (
-            <form>
-                <input type="text" placeholder="Search..." />
-            </form>
-        );
-    }
-}
+//TODO: Make the search function work with
+//      your fight finder API.
 
 class SearchModule extends React.Component {
     render() {
         return (
             <div>
                 <SearchBar />
+                <SearchResults results={this.props.results}/>
             </div>
-        )
+        );
     }
 }
 
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <form>
+                <input type="text" placeholder="Search..." />
+                <button type="button">Search</button>
+            </form>
+        );
+    }
+}
+
+class SearchResults extends React.Component {
+    render() {
+        const rows = [];
+        this.props.results.forEach((result) => {
+            rows.push(<RecordRow
+                        result={result}
+                        key={result.row_id} />);
+        });
+
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Opponent</th>
+                        <th>Outcome</th>
+                    </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+            </table>
+        );
+    }
+}
+
+class RecordRow extends React.Component {
+    render() {
+        const result = this.props.result;
+        return (
+            <tr>
+                <td>{result.opponent}</td>
+                <td>{result.outcome}</td>
+            </tr>
+        );
+    }
+}
+
+//HARD-CODE RESULTS FOR NOW
+const RESULTS = [
+  { row_id: 1, opponent: "Hulk Hogan", outcome: "W" },
+  { row_id: 2, opponent: "Ultimate Warrior", outcome: "L" },
+];
+
+
+
+
 ReactDOM.render(
-    <SearchModule />,
+    <SearchModule results={RESULTS} />,
     document.getElementById("root")
 );
 
